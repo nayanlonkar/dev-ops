9e8aa0406bb71a1da380d3c9e8f7a4155a8b48e3
Nayan Lonkar <37153812+nayanlonkar@users.noreply.github.com>
2020-04-26 15:41:33 +0530

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -2,7 +2,4 @@ CREATE DATABASE IF NOT EXISTS crud_db;
 USE crud_db;
 
 CREATE TABLE IF NOT EXISTS user_detail ( id INT AUTO_INCREMENT PRIMARY KEY, name CHAR(30) NOT NULL, age INT NOT NULL, department CHAR(25) NOT NULL, subject CHAR(50) );
-<<<<<<< HEAD
-=======
 
->>>>>>> b30c7d9accfe848ce3c83ad5c139ecd52d0de1fb
