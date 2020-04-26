18e0d46638720ee8e31d5f23112db226bf330512
Nayan Lonkar <nayanlonkar@gmail.com>
2020-04-26 15:36:31 +0530

    updated files

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,11 @@
 # pull latest mysql image
-FROM node
+FROM ubuntu
 
 # maintainer
 LABEL MAINTAINER="Nayan"
 
 RUN apt-get update && apt-get upgrade -y
-RUN apt-get install mysql-server git -y
+RUN apt-get install mysql-server git curl -y
 RUN git clone https://github.com/nayanlonkar/dev-ops
-RUN cd myApp && npm install 
-
 
 EXPOSE 8080/tcp
--- create_table.sql
+++ create_table.sql
@@ -1,11 +1,4 @@
 CREATE DATABASE IF NOT EXISTS crud_db;
 USE crud_db;
 
-CREATE TABLE IF NOT EXISTS user_detail (
-	id		INT		AUTO_INCREMENT		PRIMARY KEY,
-	name	CHAR(30)	NOT NULL,
-	age		INT 		NOT NULL,
-	department	CHAR(25)	NOT NULL,
-	subject		CHAR(50)
-);
-
+CREATE TABLE IF NOT EXISTS user_detail ( id INT AUTO_INCREMENT PRIMARY KEY, name CHAR(30) NOT NULL, age INT NOT NULL, department CHAR(25) NOT NULL, subject CHAR(50) );
