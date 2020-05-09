2c923c6b792e17f094771cf709dd842afbe4cca1
Nayan Lonkar <nayanlonkar@gmail.com>
2020-05-09 12:50:01 +0530

    post request completed

--- myApp/api/post.js
+++ myApp/api/post.js
@@ -4,6 +4,21 @@ const connection = require("../db_connection");
 
 // post method
 router.post("/", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const department = req.body.department;
+  const subject = req.body.subject;
+
+  if (
+    name == undefined ||
+    age == undefined ||
+    department == undefined ||
+    subject == undefined
+  ) {
+    res.status(400).send({ error: "parameter missing. Try again..." });
+    return;
+  }
+
   connection.query(
     "INSERT INTO user_detail SET ?",
     [req.body],
